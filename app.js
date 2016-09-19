dd1e6f9e127d268bb5992c7cbd6f279f2b61e1e8
Aliot <aliot@tut.by>
2016-09-19 12:53:19 +0300

    new commit

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
-angular.module("studyModule", [])
-.controller("studyCtrl", function ($scope) {
-	$scope.tasks = [
-		{ action: "Personalia", complete: 'data="about"' },
-		{ action: "Doświadczenie", complete: 'data="experience"' },
-		{ action: "Umiejętności", complete: 'data="skills"' },		
-		{ action: "Języki", complete: 'data="languages"' },		
-		{ action: "Zainteresowania", complete: 'data="hobbies"' }
-	];
+'use strict';
+(function(){
+	var app = angular.module('app', ['ngMaterial']);
+
+	app.controller('AppCtrl', function($scope, $mdDialog) {
+    $scope.user = {
+        firstName: '',
+        lastName: '',
+        country: '',
+        city: ''        
+    };
+    $scope.users = [];
+    $scope.save = function() {
+        $scope.users.push($scope.user);
+        $scope.user = {
+            firstName: '',
+            lastName: '',
+            country: '',
+            city: ''
+        };
+    }
+    $scope.delete = function(index, ev) {
+        var confirm = $mdDialog.confirm()
+            .title('Czy chcesz usunąć dane ?')
+            .content('W przypadku zgody dane zostaną całkowicie usunięte z listy.')
+            .ok('Usunąć!')
+            .cancel('Anulować')
+            .targetEvent(ev);
+        $mdDialog.show(confirm).then(function() {
+            $scope.users.splice(index, 1);
+        }, function() {});
+    }
 });
- 
+
+
+})(); 
