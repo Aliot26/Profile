0fd5ec090c34a86121bea2619e12aa0d843a7952
Aliot <aliot@tut.by>
2016-09-20 17:33:55 +0300

    Tuesday's commit

--- app.js
+++ app.js
@@ -10,15 +10,20 @@
         city: ''        
     };
     $scope.users = [];
-    $scope.save = function() {
-        $scope.users.push($scope.user);
+    $scope.saveData = function(newUser) {
+		if(angular.isDefined(newUser) && angular.isDefined(newUser.firstName) && angular.isDefined(newUser.lastName) && angular.isDefined(newUser.country)  && angular.isDefined(newUser.city)){
+        $scope.users.push({firstName:newUser.firstName, lastName:newUser.lastName, country:newUser.country, city:newUser.city});
         $scope.user = {
             firstName: '',
             lastName: '',
             country: '',
             city: ''
         };
+		};
     }
+	$scope.limitValue = "5";
+    $scope.limitRange = [1,2,3,4,5];
+	
     $scope.delete = function(index, ev) {
         var confirm = $mdDialog.confirm()
             .title('Czy chcesz usunąć dane ?')
@@ -31,6 +36,24 @@
         }, function() {});
     }
 });
+	app.controller('CheckCtrl', function($scope) {
 
+    $scope.items = ["PHP","HTML","CSS","JS","C++","C#","JAVA","PYTHON"];
+      $scope.selected = [];
+
+      $scope.toggle = function (item, list) {
+        var idx = list.indexOf(item);
+        if (idx > -1) {
+          list.splice(idx, 1);
+        }
+        else {
+          list.push(item);
+        }
+      };
+
+      $scope.exists = function (item, list) {
+        return list.indexOf(item) > -1;
+      };
+});
 
 })(); 
