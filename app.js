2d036a23928ccfd2fbb7218edb0581653071decc
Aliot <aliot@tut.by>
2016-09-22 12:28:10 +0300

    final commit

--- app.js
+++ app.js
@@ -36,6 +36,23 @@
         }, function() {});
     }
 });
+
+	app.directive("workList", function () {
+			return {
+				link: function (scope, element, attributes) {
+					scope.data = scope[attributes["workList"]];
+				},
+				restrict: "A",
+				template: "<ul><li ng-repeat='item in data'>{{item.itemName}}&nbsp;-&nbsp;{{item.itemPosition}}</li></ul>"
+			}
+		})
+	app.controller("WorkCtrl", function ($scope) {
+		$scope.items = [
+			{ itemName: "Internet agency Web24", itemPosition: "Web Developer" },
+			{ itemName: "Belarusbank", itemPosition: "Specialist" }			
+		];
+	});
+
 	app.controller('CheckCtrl', function($scope) {
 
     $scope.items = ["PHP","HTML","CSS","JS","C++","C#","JAVA","PYTHON"];
@@ -56,4 +73,72 @@
       };
 });
 
+	app.directive('mySlider',function(){
+		return {			
+			link: function (scope, element, attrs) {			
+					element.on('mouseup', function () {					
+					switch(scope.rating3){
+						case 1:
+							angular.element(document.querySelector('#eng')).text("podstawowy");
+							break;
+						case 2:
+							angular.element(document.querySelector('#eng')).text("śriedni");
+							break;
+						case 3:
+							angular.element(document.querySelector('#eng')).text("średnio-zaawansowany");
+							break;
+						case 4:
+							angular.element(document.querySelector('#eng')).text("zaawansowany");
+							break;
+						case 5:
+							angular.element(document.querySelector('#eng')).text("ojczysty");
+							break;
+						default:
+							angular.element(document.querySelector('#eng')).text("Niema poziomu");
+					};
+				
+				});
+			}
+		};	
+	});
+
+	app.controller('sliderCtrl', function($scope) {
+		$scope.rating1 = 3;
+		$scope.rating2 = 2;
+		$scope.rating3 = 2;
+});
+
+
+ /*   app.directive('mySlider', function() {
+	   return {		 
+		 link: function(scope, element, attrs) {
+			var sliderFunc = function(aria-valuenow) {
+			
+				if (aria-valuenow == "4") {
+					console.log('click');
+				}
+										switch(parent.scope.rating3){
+						case '1':
+							console.log('1');
+							break;
+						case '2':
+							console.log('2');
+							break;
+						case '3':
+							console.log('3');
+							break;
+						case '4':
+							console.log('4');
+							break;
+						case '5':
+							console.log('5');
+							break;
+					}	
+				
+			 }
+			 
+			 
+		}
+	  };
+	})*/
 })(); 
